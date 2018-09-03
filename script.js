584de9ef25eed1a7a5ed5b3a782cfab7033d3f73
Adrian <adrian@netkata.com>
2018-09-03 16:02:54 +0200

    first steps

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const endpoint = 'https://raw.githubusercontent.com/ajotka/JSONs/master/countries.json';
+
+// var request = new XMLHttpRequest();
+//
+// request.open('GET', endpoint, true);
+//
+// request.onload = function () {
+//     var data = JSON.parse(this.response);
+//
+//     console.log(data);
+// };
+//
+// request.send();
+
+fetch(endpoint)
+    .then(function (res) {
+        return  res.json();
+})
+    .then(function (data) {
+        console.log(data[0]);
+})
