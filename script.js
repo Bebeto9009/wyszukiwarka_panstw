de0abc756bf430be074677816adfda82b9797217
Adrian <adrian@netkata.com>
2018-09-04 16:34:56 +0200

    something more in code than before

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 const endpoint = 'https://raw.githubusercontent.com/ajotka/JSONs/master/countries.json';
-
-// var request = new XMLHttpRequest();
-//
-// request.open('GET', endpoint, true);
-//
-// request.onload = function () {
-//     var data = JSON.parse(this.response);
-//
-//     console.log(data);
-// };
-//
-// request.send();
+var obj = 0;
 
 fetch(endpoint)
     .then(function (res) {
         return  res.json();
 })
     .then(function (data) {
-        console.log(data[0]);
-})
+        obj = data;
+        console.log(obj);
+});
+
+var searchInput = document.querySelector(".search-input");
+
+searchInput.addEventListener('keyup', (e) => {
+    console.log("keyup działa poprawnie");
+    console.log(searchInput.value);
+    searchEngine();
+});
+
+
+function searchEngine() {
+    console.log('searchEngine working! długość tablicy: ' + obj.length);
+    for (var i = 0; i < obj.length; i++) {
+        if (obj[i].name.includes(searchInput.value)) {
+            var results = [];
+            results += obj[i].name;
+            return console.log('zwraca ' + results);
+        }
+
+    }
+
+};
+
+obj.forEach(searchEngine);
+console.log(searchInput.value);
+
+var resultDetails = document.querySelector('.result__details');
+var resultSubtitle = document.querySelector('.result__subtitle');
