14996eecf7dbab365db1b2d49e32ec848ed0de56
bebeto9009 <adr.pawlak@gmail.com>
2018-09-09 00:15:05 +0200

    some improvements

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const endpoint = 'https://raw.githubusercontent.com/ajotka/JSONs/master/countries.json';
-var obj = 0;
+let obj = [];
+let results = [];
+let countriesArr = 0;
 
 fetch(endpoint)
     .then(function (res) {
@@ -7,48 +9,62 @@ fetch(endpoint)
 })
     .then(function (data) {
         obj = data;
-        // console.log(obj);
+        countriesArr = obj.map(el => Object.values(el));
+        // console.log(countriesArr);
 });
 
-var searchInput = document.querySelector(".search-input");
+const searchInput = document.querySelector(".search-input");
 
 searchInput.addEventListener('keyup', (e) => {
-    // console.log("keyup działa poprawnie");
-    // console.log(searchInput.value);
+    results.length = [];
     searchEngine();
 });
 
-// function finalResults () {
-//
-// }
-
-var results = [];
-
 function searchEngine() {
-    console.log('wartość searchInput: ' + searchInput.value);
+    for (let i = 0; i < countriesArr.length; i++){
 
-    for (var i = 0; i < obj.length; i++) {
-        if (searchInput.value == '') {
-            results = [];
-            resultDetails.innerHTML = '';
-            console.log('results from if: ' + results)
-        } else if (obj[i].name.includes(searchInput.value) && !results.includes(obj[i].name)) {
-            // results += obj[i].name;
-            results.push(obj[i].name);
-            resultDetails.innerHTML += results;
-            return console.log('pierwszy else if zwraca: ' + results);
-        } else if (obj[i].name.includes(searchInput.value) && results.includes(obj[i].name)) {
-            for (var j = 0; j < results.length; j++) {
-                if (results.includes(searchInput.value)){
-                    results = [];
+        resultDetails.innerHTML = results;
+            if (countriesArr[i][0].toLowerCase().includes(searchInput.value.toLowerCase())) {
+                if (results.includes(countriesArr[i])) {
+                    return;
+                } else {
+                    results.push(countriesArr[i]);
+                    console.log('zawiera! ' + countriesArr[i]);
+                    console.log('zawiera results!: ' + results);
                     resultDetails.innerHTML += results;
-                    console.log('drugi else if: ' + results);
-
                 }
             }
         }
-    }
 };
+
+
+
+// function searchEngine() {
+//     console.log('wartość searchInput: ' + searchInput.value);
+//
+//     for (var i = 0; i < obj.length; i++) {
+//         if (searchInput.value == '') {
+//             results = [];
+//             resultDetails.innerHTML = '';
+//             console.log('results from if: ' + results)
+//         } else if (obj[i].name.includes(searchInput.value) && !results.includes(obj[i].name)) {
+//             // results += obj[i].name;
+//             results = [];
+//             results.push(obj[i].name);
+//             resultDetails.innerHTML += results;
+//             return console.log('pierwszy else if zwraca: ' + results);
+//         } else if (obj[i].name.includes(searchInput.value) && results.includes(obj[i].name)) {
+//             for (var j = 0; j < results.length; j++) {
+//                 if (results.includes(searchInput.value)){
+//                     results = [];
+//                     resultDetails.innerHTML += results;
+//                     console.log('drugi else if: ' + results);
+//
+//                 }
+//             }
+//         }
+//     }
+// };
 
 // function searchEngine() {
 //     // console.log('searchEngine working! długość tablicy: ' + obj.length);
@@ -70,3 +86,4 @@ function searchEngine() {
 
 var resultDetails = document.querySelector('.result__details');
 var resultSubtitle = document.querySelector('.result__subtitle');
+
